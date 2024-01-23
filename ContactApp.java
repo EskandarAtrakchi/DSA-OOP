7ff11901a4784ed24f82717f94620a04c97e96f3
Eskandar Atrakchi <102361045+EskandarAtrakchi@users.noreply.github.com>
2024-01-23 10:49:53 +0000

    code updated

---
+++ ContactApp.java
@@ -0,0 +1,20 @@
+package contact;
+
+/**
+ *
+ * @author Eskandar Atrakchi 23137517
+ */
+public class ContactApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        ContactUI cui = new ContactUI () ;
+        
+        cui.setVisible(true);
+        
+    }
+    
+}
